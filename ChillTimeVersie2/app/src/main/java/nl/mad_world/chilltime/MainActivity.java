8713869c660a1288eb4715d2a44546146b0db7a8
oveldman <oveldman@gmail.com>
2015-05-28 13:15:59 +0200

    Verbeter  login

--- ChillTimeVersie2/app/src/main/java/nl/mad_world/chilltime/MainActivity.java
+++ ChillTimeVersie2/app/src/main/java/nl/mad_world/chilltime/MainActivity.java
@@ -1,9 +1,11 @@
 package nl.mad_world.chilltime;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -35,4 +37,11 @@ public class MainActivity extends ActionBarActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+    public void Login(View view)    {
+        Intent intent = new Intent(this, Groep.class);
+
+        startActivity(intent);
+    }
+
 }
