e2b6d0abe72c37a73b81bca69e13330a923950f2
oveldman <oveldman@gmail.com>
2015-05-28 14:20:31 +0200

    Verwijder en new

--- ChillTimeVersion2/app/src/main/java/nl/mad_world/chilltime/Groep.java
+++ ChillTimeVersion2/app/src/main/java/nl/mad_world/chilltime/Groep.java
@@ -1,25 +1,23 @@
 package nl.mad_world.chilltime;
 
-import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 
 
-public class MainActivity extends ActionBarActivity {
+public class Groep extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_groep);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.menu_groep, menu);
         return true;
     }
 
@@ -37,11 +35,4 @@ public class MainActivity extends ActionBarActivity {
 
         return super.onOptionsItemSelected(item);
     }
-
-    public void Login(View view)    {
-        Intent intent = new Intent(this, Groep.class);
-
-        startActivity(intent);
-    }
-
 }
