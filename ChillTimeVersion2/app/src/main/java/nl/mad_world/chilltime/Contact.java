199eaff4118664422bb6fa7d0facf8e94a2e1b94
oveldman <oveldman@gmail.com>
2015-06-02 14:37:57 +0200

    Rotate off

--- ChillTimeVersion2/app/src/main/java/nl/mad_world/chilltime/Contact.java
+++ ChillTimeVersion2/app/src/main/java/nl/mad_world/chilltime/Contact.java
@@ -9,6 +9,8 @@ import android.view.Menu;
 import android.widget.ListView;
 import android.widget.ArrayAdapter;
 
+import java.util.Arrays;
+
 
 public class Contact extends ActionBarActivity {
 
@@ -22,11 +24,13 @@ public class Contact extends ActionBarActivity {
         setContentView(R.layout.activity_contact);
 
         stringArray = new String[10];
-        stringArray[0] = "Oscar";
-        stringArray[1] = "Roy";
-        for(int i=2; i < stringArray.length; i++){
+        stringArray[0] = "Oscar Veldman";
+        stringArray[1] = "Roy van den Heuvel";
+        stringArray[2] = "Johan Bos";
+        for(int i=3; i < stringArray.length; i++){
             stringArray[i] = "String " + i;
         }
+        Arrays.sort(stringArray);
         tweetItemArrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, stringArray);
         tweetListView = (ListView) findViewById(R.id.listViewID);
         tweetListView.setAdapter(tweetItemArrayAdapter);
