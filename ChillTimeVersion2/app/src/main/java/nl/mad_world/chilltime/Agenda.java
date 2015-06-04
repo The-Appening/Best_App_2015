4c773b8a35c81114cf2b4c236b8cc011a6c1c61b
JHNBOS <bosbosjohan@gmail.com>
2015-06-04 14:46:27 +0200

    Agenda normaal view

--- ChillTimeVersion2/app/src/main/java/nl/mad_world/chilltime/Agenda.java
+++ ChillTimeVersion2/app/src/main/java/nl/mad_world/chilltime/Agenda.java
@@ -3,10 +3,6 @@ package nl.mad_world.chilltime;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.view.View.OnClickListener;
-import android.widget.Button;
-import android.widget.DatePicker;
 import android.widget.Toast;
 
     public class Agenda extends Activity {
@@ -21,20 +17,6 @@ import android.widget.Toast;
         public void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
             setContentView(R.layout.main);
-
-            Button openButton = (Button) findViewById(R.id.openButton);
-
-            openButton.setOnClickListener(new OnClickListener() {
-
-                @Override
-                public void onClick(View v) {
-                    DatePicker dp = (DatePicker) findViewById(R.id.datePicker1);
-                    Intent intent = new Intent(v.getContext(), CalendarView.class);
-
-                    intent.putExtra("date", dp.getYear() + "-" + dp.getMonth() + "-" + dp.getDayOfMonth());
-                    startActivityForResult(intent, PICK_DATE_REQUEST);
-                }
-            });
         }
 
         @Override
@@ -44,8 +26,7 @@ import android.widget.Toast;
                 if (resultCode == RESULT_OK) {
                     Toast.makeText(getApplicationContext(), data.getStringExtra("date"), Toast.LENGTH_SHORT).show();
                     String[] dateArr = data.getStringExtra("date").split("-");
-                    DatePicker dp = (DatePicker) findViewById(R.id.datePicker1);
-                    dp.updateDate(Integer.parseInt(dateArr[0]), Integer.parseInt(dateArr[1]), Integer.parseInt(dateArr[2]));
+
                 }
             }
         }
