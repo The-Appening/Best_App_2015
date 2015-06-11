029b738232590f522a571aa8e39387f94d00214c
oveldman <oveldman@gmail.com>
2015-06-11 13:42:22 +0200

    Zoek contact

--- ChillTimeVersion2/app/src/main/java/nl/mad_world/chilltime/addContact.java
+++ ChillTimeVersion2/app/src/main/java/nl/mad_world/chilltime/addContact.java
@@ -1,30 +1,24 @@
 package nl.mad_world.chilltime;
 
-import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 import com.parse.FindCallback;
-import com.parse.Parse;
 import com.parse.ParseException;
-import com.parse.ParseObject;
 import com.parse.ParseQuery;
+import com.parse.ParseUser;
 
 import java.util.ArrayList;
 import java.util.List;
 
 
 public class addContact extends ActionBarActivity {
-
+    ArrayList<String> ContactAdd = new ArrayList<>();
     private ListView monthsListView;
     private ArrayAdapter arrayAdapter;
-    private ArrayList ContactList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,30 +27,29 @@ public class addContact extends ActionBarActivity {
     }
 
     public void SearchContact(View view){
-        monthsListView = (ListView) findViewById(R.id.AddContactList);
         getContactData();
+        monthsListView = (ListView) findViewById(R.id.AddContactList);
 
         // this-The current activity context.
         // Second param is the resource Id for list layout row item
         // Third param is input array
-        arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, ContactList);
+        arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, ContactAdd);
         monthsListView.setAdapter(arrayAdapter);
     }
 
     public void getContactData(){
-        ParseQuery<ParseObject> query = ParseQuery.getQuery("User");
-        query.whereEqualTo("Voornaam", "roy");
-        query.findInBackground(new FindCallback<ParseObject>() {
-            public void done(List<ParseObject> scoreList, ParseException e) {
+        ParseQuery<ParseUser> query = ParseUser.getQuery();
+       // query.whereEqualTo("username","*");
+        query.findInBackground(new FindCallback<ParseUser>() {
+            public void done(List<ParseUser> objects, ParseException e) {
                 if (e == null) {
-                    Log.d("score", "Retrieved " + scoreList.size() + " scores");
+                    for (int i = 0; i < objects.size(); i++) {
+                        ContactAdd.add(objects.get(i).getString("username"));
+                    }
                 } else {
-                    Log.d("score", "Error: " + e.getMessage());
+                        ContactAdd.add("Er zijn geen gebruikers gevonden");
                 }
 
-                for(int i = 0; i > scoreList.size(); i++){
-                    ContactList.add(scoreList.get(i));
-                }
             }
         });
     }
