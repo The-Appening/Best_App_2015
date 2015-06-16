864b49309d390d85276adab311ca81064d7d4e24
RoyvandenHeuvel <0878963@hr.nl>
2015-06-16 15:17:42 +0200

    groepmaker wordt opgeslagen, voor altijd, muwhahahahaha

--- ChillTimeVersion2/app/src/main/java/nl/mad_world/chilltime/AddGroup.java
+++ ChillTimeVersion2/app/src/main/java/nl/mad_world/chilltime/AddGroup.java
@@ -1,11 +1,8 @@
 package nl.mad_world.chilltime;
 
-import android.app.AlertDialog;
 import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
 
@@ -23,11 +20,15 @@ public class AddGroup extends ActionBarActivity {
 
     public void SaveGroup(View view) {
         ParseObject group = new ParseObject("Groups");
+        ParseUser currentUser = ParseUser.getCurrentUser();
+
+        String username = currentUser.getUsername();
 
         EditText gNameET = (EditText) findViewById(R.id.groupName);
         String groupName = gNameET.getText().toString();
 
         group.put("Name", groupName);
+        group.put("Creator", username);
         group.saveInBackground();
 
         Intent intent = new Intent(this, Group.class);
