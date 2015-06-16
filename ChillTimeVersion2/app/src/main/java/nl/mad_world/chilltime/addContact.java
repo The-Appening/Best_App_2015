cfcbd9167302696f73b531f38e8c9633c6c1b1c3
oveldman <oveldman@gmail.com>
2015-06-16 11:50:41 +0200

    AddContact
    AddContact

--- ChillTimeVersion2/app/src/main/java/nl/mad_world/chilltime/addContact.java
+++ ChillTimeVersion2/app/src/main/java/nl/mad_world/chilltime/addContact.java
@@ -2,8 +2,11 @@ package nl.mad_world.chilltime;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.view.ContextMenu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.ListView;
 
 import com.parse.FindCallback;
@@ -34,12 +37,16 @@ public class addContact extends ActionBarActivity {
         // Second param is the resource Id for list layout row item
         // Third param is input array
         arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, ContactAdd);
+        registerForContextMenu(monthsListView);
         monthsListView.setAdapter(arrayAdapter);
     }
 
     public void getContactData(){
+        ContactAdd.clear();
+        EditText sNameET = (EditText) findViewById(R.id.ContactName);
+        String ContactNames = sNameET.getText().toString();
         ParseQuery<ParseUser> query = ParseUser.getQuery();
-       // query.whereEqualTo("username","*");
+        query.whereEqualTo("username", ContactNames);
         query.findInBackground(new FindCallback<ParseUser>() {
             public void done(List<ParseUser> objects, ParseException e) {
                 if (e == null) {
@@ -47,11 +54,30 @@ public class addContact extends ActionBarActivity {
                         ContactAdd.add(objects.get(i).getString("username"));
                     }
                 } else {
-                        ContactAdd.add("Er zijn geen gebruikers gevonden");
+                    ContactAdd.add("Er zijn geen gebruikers gevonden");
                 }
 
             }
         });
+    }
+
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        super.onCreateContextMenu(menu, v, menuInfo);
+        menu.setHeaderTitle("Contact");
+        menu.add(0, v.getId(), 0, "Toevoegen");
+    }
+
+    @Override
+    public boolean onContextItemSelected(MenuItem item) {
+        if(item.getTitle()=="Toevoegen"){
+            //TODO maken functie die contact verwijdert.
+            // voorbeeldfunctienaam(item.getItemId());
+        }
+        else {
+            return false;
+        }
+        return true;
     }
 
 }
