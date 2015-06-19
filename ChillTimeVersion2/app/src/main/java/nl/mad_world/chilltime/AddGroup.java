4c1b6ef98a0f003c8109915a45452fcce3d30b4a
RoyvandenHeuvel <0878963@hr.nl>
2015-06-19 12:26:59 +0200

    1 klik om groep te registreren yay

--- ChillTimeVersion2/app/src/main/java/nl/mad_world/chilltime/AddGroup.java
+++ ChillTimeVersion2/app/src/main/java/nl/mad_world/chilltime/AddGroup.java
@@ -4,7 +4,6 @@ import android.app.AlertDialog;
 import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 
@@ -19,8 +18,7 @@ import java.util.List;
 
 public class AddGroup extends ActionBarActivity {
 
-    boolean Success;
-    boolean isntWaiting;
+    String groupName = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,12 +27,6 @@ public class AddGroup extends ActionBarActivity {
     }
 
     public void SaveGroup(View view) {
-        ParseObject group = new ParseObject("Groups");
-        ParseObject groupMembers = new ParseObject("Group_members");
-        ParseUser currentUser = ParseUser.getCurrentUser();
-
-        String username = currentUser.getUsername();
-
         EditText gNameET = (EditText) findViewById(R.id.groupName);
         String groupName = gNameET.getText().toString();
 
@@ -44,40 +36,30 @@ public class AddGroup extends ActionBarActivity {
         check.findInBackground(new FindCallback<ParseObject>() {
             public void done(List<ParseObject> objects, ParseException e) {
                 if (e == null) {
-                    if (objects.size()==0) {
-                        Success = true;
-                        isntWaiting = true;
+                    if (objects.size() == 0) {
+                        register();
                     }
-                    else{
-                        Success = false;
-                        isntWaiting = true;
-                    }
-                } else {
-                    Success = false;
-                    isntWaiting = true;
                 }
             }
         });
+    }
 
-        if(isntWaiting) {
-            if (Success == true) {
-                group.put("Name", groupName);
-                group.put("Creator", username);
-                group.saveInBackground();
+    public void register() {
+        ParseUser currentUser = ParseUser.getCurrentUser();
+        String username = currentUser.getUsername();
 
-                groupMembers.put("Username", username);
-                groupMembers.put("GroupName", groupName);
-                groupMembers.saveInBackground();
+        ParseObject group = new ParseObject("Groups");
+        ParseObject groupMembers = new ParseObject("Group_members");
 
-                Intent intent = new Intent(this, Group.class);
-                startActivity(intent);
-            } else {
-                new AlertDialog.Builder(this)
-                        .setMessage("De door u ingevulde groepsnaam is al in gebruik.")
-                        .setIcon(android.R.drawable.ic_dialog_alert)
-                        .setCancelable(true)
-                        .show();
-            }
-        }
+        group.put("Name", groupName);
+        group.put("Creator", username);
+        group.saveInBackground();
+
+        groupMembers.put("Username", username);
+        groupMembers.put("GroupName", groupName);
+        groupMembers.saveInBackground();
+
+        Intent intent = new Intent(this, Group.class);
+        startActivity(intent);
     }
 }
