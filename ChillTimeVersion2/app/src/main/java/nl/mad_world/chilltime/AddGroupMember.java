e0a7c14734a230ccc3db516790f10984c3f1c211
RoyvandenHeuvel <0878963@hr.nl>
2015-06-23 15:10:53 +0200

    mensen kan je nu toevoegen aan groepen

--- ChillTimeVersion2/app/src/main/java/nl/mad_world/chilltime/AddGroupMember.java
+++ ChillTimeVersion2/app/src/main/java/nl/mad_world/chilltime/AddGroupMember.java
@@ -1,16 +1,27 @@
 package nl.mad_world.chilltime;
 
+import android.content.Context;
 import android.content.Intent;
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.ActionBarActivity;
+import android.view.ContextMenu;
+import android.view.ContextMenu.ContextMenuInfo;
 import android.view.LayoutInflater;
 import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import com.parse.FindCallback;
 import com.parse.ParseException;
@@ -20,6 +31,7 @@ import com.parse.ParseUser;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 
 
 public class AddGroupMember extends ActionBarActivity {
@@ -33,6 +45,32 @@ public class AddGroupMember extends ActionBarActivity {
         getGroupData();
         ListView listView = (ListView) this.findViewById(R.id.ListView);
         listView.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, Groups));
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> arg0, View arg1,
+                                    int position, long arg3) {
+
+                Intent intent = getIntent();
+
+                String toAddMember = intent.getExtras().getString("NewMember");
+
+                addAMember(Groups.get(position), toAddMember);
+
+                Intent intent2 = new Intent(AddGroupMember.this, Group.class);
+                startActivity(intent2);
+
+
+            }
+        });
+    }
+
+    public void addAMember(String groupName, String toAddMember){
+
+        ParseObject groupMembers = new ParseObject("Group_members");
+        groupMembers.put("Username", toAddMember);
+        groupMembers.put("GroupName", groupName);
+        groupMembers.saveInBackground();
     }
 
     public void getGroupData() {
